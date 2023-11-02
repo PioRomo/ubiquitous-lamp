21252cd139981488419b4f81fce81ce29d827e34
Pio L Romo <104649165+PioRomo@users.noreply.github.com>
2023-11-02 11:53:51 -0700

    Update Doc.java

--- Doc.java
+++ Doc.java
@@ -1,1 +1,9 @@
 
+Error: Cannot find module '/Users/lencho/Downloads/LikeHome-mk1/client/.yarn/releases/yarn-1.22.19.cjs'
+    at Module._resolveFilename (node:internal/modules/cjs/loader:1048:15)
+    at Module._load (node:internal/modules/cjs/loader:901:27)
+    at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:83:12)
+    at node:internal/main/run_main_module:23:47 {
+  code: 'MODULE_NOT_FOUND',
+  requireStack: []
+}
